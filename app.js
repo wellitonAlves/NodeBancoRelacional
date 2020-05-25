aebf457186185d77c196038589695fc6608ee382
jose <josealves_w@yahoo.com.br>
2020-05-24 23:21:35 -0300

    adicionando arquivo html

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req,res){
-    res.send("bem vindo a palicacao");
+    res.sendFile(__dirname+"/html/index.html");
 })
 
 app.get("/sobre", function(req,res){
