2bfd6c654c83b96096312fb8ace93f7e00d7709a
jose <josealves_w@yahoo.com.br>
2020-05-25 01:24:24 -0300

    trabalhando com formularios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars")
 const Sequelize = require('sequelize')
+const bodyParser = require('body-parser')
 
 //conexão com o banco de dados mysql
 const sequelize = new Sequelize("node","welliton", "root",{
@@ -9,10 +10,25 @@ const sequelize = new Sequelize("node","welliton", "root",{
     dialect:"mysql"
 })
 
+//body parser
+app.use(bodyParser.urlencoded({extends:false}))
+app.use(bodyParser.json())
 
 //config TemplateEngine
     app.engine("handlebars", handlebars({defaultLayout:"main"}))
     app.set("view engine", "handlebars")
+
+
+
+    app.get("/cadastro", function(req,res){
+        res.render('formulario')
+    })
+
+
+    app.post("/add", function(req,res){
+        res.send("formulario recebido "+req.body.titulo +" conteudo: "+req.body.conteudo)
+    })
+
 
 app.listen(8081, function(){
     console.log("Servidor rodando na porta 8081")
